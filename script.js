e34f6b08984fbff5ff036658ae5c85eb8279ebab
Ahsaan099 <160276367+Ahsaan099@users.noreply.github.com>
2026-06-30 16:28:39 +0500

    Initial commit

---
+++ script.js
@@ -0,0 +1,23 @@
+function changeColorRed() {
+  document.body.style.backgroundColor = "#FFE5E5"; 
+}
+
+function changeColorBlue() {
+  document.body.style.backgroundColor = "#E6F4FF"; 
+}
+
+function changeColorGreen() {
+  document.body.style.backgroundColor = "#E8F5E9"; 
+}
+
+function changeColorOrange() {
+  document.body.style.backgroundColor = "#FFF3E6"; 
+}
+
+function changeColorYellow() {
+  document.body.style.backgroundColor = "#FFFBE6"; 
+}
+
+function changeColor() {
+  document.body.style.backgroundColor = "#FFFFFF"; 
+}
